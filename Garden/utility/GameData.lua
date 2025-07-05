802870d0f68eca7b4cc2cc880c640f48659b2d23
NoLag <153074411+NoLag-id@users.noreply.github.com>
2025-07-05 21:02:02 +0700

    Update GameData.lua

--- Garden/utility/GameData.lua
+++ Garden/utility/GameData.lua
@@ -1,25 +1,25 @@
 local data = {
-  fruit = { "All Selection", "Aloe Vera", "Apple", "Avocado", "Banana", "Bamboo", "Beanstalk", "Bee Balm", "Bell Pepper", "Bendboo", "Blood Banana", "Blue Lollipop", "Blueberry", "Burning Bud", "Cacao", "Cactus", "Candy Blossom", "Candy Sunflower", "Cantaloupe", "Carrot", "Cauliflower", "Celestiberry", "Cherry Blossom", "Chocolate Carrot", "Cocovine", "Coconut", "Corn", "Cranberry", "Crimson Vine", "Crocus", "Cursed Fruit", "Daffodil", "Dandelion", "Delphinium", "Dragon Fruit", "Dragon Pepper", "Durian", "Easter Egg", "Eggplant", "Elephant Ears", "Ember Lily", "Feijoa", "Foxglove", "Grape", "Green Apple", "Guanabana", "Hive Fruit", "Honeysuckle", "Ice Cream Bean", "Kiwi", "Lavender", "Lemon", "Lilac", "Lily of the Valley", "Lime", "Loquat", "Lotus", "Lumira", "Manuka Flower", "Mango", "Mega Mushroom", "Mint", "Moon Blossom", "Moon Melon", "Moon Mango", "Moonflower", "Moonglow", "Mushroom", "Nectar Thorn", "Nectarine", "Nectarshade", "Noble Flower", "Nightshade", "Orange Tulip", "Papaya", "Parasol Flower", "Passionfruit", "Peach", "Pepper", "Peace Lily", "Pear", "Pineapple", "Pink Lily", "Pink Tulip", "Pitcher Plant", "Prickly Pear", "Pumpkin", "Purple Cabbage", "Purple Dahlia", "Rafflesia", "Raspberry", "Red Lollipop", "Rose", "Rosy Delight", "Soul Fruit", "Starfruit", "Strawberry", "Succulent", "Sugar Apple", "Suncoil", "Sunflower", "Tomato", "Traveler's Fruit", "Venus Fly Trap", "Violet Corn", "Watermelon", "White Mulberry", "Wild Carrot" },
-  mutation = { "Any", "Alienlike", "Aurora", "Bloodlit", "Burnt", "Celestial", "Chilled", "Cloudtouched", "Choc", "Cooked", "Dawnbound", "Drenched", "Disco", "Frozen", "Galactic", "Heavenly", "HoneyGlazed", "Meteoric", "Molten", "Moonlit", "Paradisal", "Plasma", "Pollinated", "Shocked", "Sundried", "Twisted", "Verdant", "Voidtouched", "Wet", "Windstruck", "Wiltproof", "Zombified" },
-  pet = {
-    "Anti Bee", "Bear Bee", "Bee", "Black Bunny", "Blood Hedgehog",
-    "Blood Kiwi", "Blood Owl", "Brown Mouse", "Bunny", "Butterfly",
-    "Capybara", "Cat", "Caterpillar", "Chicken", "Chicken Zombie",
-    "Cooked Owl", "Cow", "Crab", "Deer", "Disco Bee",
-    "Dog", "Dragonfly", "Echo Frog", "Firefly", "Flamingo",
-    "Frog", "Giant Ant", "Golden Bee", "Golden Lab", "Grey Mouse",
-    "Hedgehog", "Honey Bee", "Hamster", "Kiwi", "Mimic Octopus",
-    "Mole", "Monkey", "Moon Cat", "Moth", "Night Owl",
-    "Orange Tabby", "Orangutan", "Ostrich", "Owl", "Pack Bee",
-    "Panda", "Peacock", "Petal Bee", "Pig", "Polar Bear",
-    "Praying Mantis", "Queen Bee", "Raccoon", "Red Dragon", "Red Fox",
-    "Red Giant Ant", "Rooster", "Scarlet Macaw", "Sea Otter", "Sea Turtle",
-    "Seagull", "Seal", "Silver Monkey", "Snail", "Spotted Deer",
-    "Squirrel", "Starfish", "Tarantula Hawk", "Toucan", "Turtle",
-    "Wasp"
+  fruit = {"All Selection" ,"Aloe Vera", "Apple", "Avocado", "Bamboo", "Banana", "Banana", "Beanstalk", "Bee Balm", "Bell Pepper", "Bendboo", "Blood Banana", "Blue Lollipop", "Blueberry", "Bone Blossom", "Boneboo", "Burning Bud", "Cacao", "Cactus", "Cantaloupe", "Carrot", "Cauliflower", "Celestiberry", "Ceramic", "Cherry Blossom", "Chocolate Carrot", "Cocovine", "Coconut", "Cranberry", "Crimson Vine", "Crocus", "Cursed Fruit", "Daffodil", "Dandelion", "Delphinium", "Dragon Fruit", "Dragon Pepper", "Durian", "Easter Egg", "Eggplant", "Elephant Ears", "Ember Lily", "Feijoa", "Firefly Fern", "Firework Flower", "Foxglove", "Fossilight", "Grape", "Green Apple", "Guanabana", "Hive Fruit", "Honeysuckle", "Horned Dinoshroom", "Ice Cream Bean", "Kiwi", "Lavender", "Lemon", "Liberty Lily", "Lilac", "Lily of the Valley", "Lime", "Loquat", "Lotus", "Lumira", "Manuka Flower", "Mango", "Mega Mushroom", "Merica Mushroom", "Mint", "Moon Blossom", "Moon Melon", "Moon Mango", "Moonflower", "Moonglow", "Mushroom", "Nectar Thorn", "Nectarine", "Nectarshade", "Noble Flower", "Orange Tulip", "Papaya", "Paradise Petal", "Parasol Flower", "Passionfruit", "Peace Lily", "Peach", "Pepper", "Pear", "Pineapple", "Pink Lily", "Pink Tulip", "Pitcher Plant", "Plasma", "Prickly Pear", "Pumpkin", "Purple Cabbage", "Purple Dahlia", "Rafflesia", "Raspberry", "Red Lollipop", "Rose", "Rosy Delight", "Soul Fruit", "Stonebite", "Starfruit", "Strawberry", "Sugar Apple", "Succulent", "Suncoil", "Sunflower", "Traveler's Fruit", "Tomato", "Venus Fly Trap", "Violet Corn", "Watermelon", "White Mulberry", "Wild Carrot"},
+  mutation = {
+    "Any", "Alienlike", "Amber", "AncientAmber", "Aurora", "Bloodlit", "Burnt", "Celestial", "Chilled", "Choc", "Clay",
+    "Cloudtouched", "Cooked", "Dawnbound", "Disco", "Drenched", "Fried", "Frozen", "Galactic", "Heavenly",
+    "HoneyGlazed", "Meteoric", "Molten", "Moonlit", "OldAmber", "Paradisal", "Plasma", "Pollinated", "Sandy",
+    "Shocked", "Sundried", "Twisted", "Verdant", "Voidtouched", "Wet", "Wiltproof", "Windstruck", "Zombified"
   },
-  crate = {"Mysterious Crate", "Sign Crate", "Common Gnome Crate", "Farmers Gnome Crate", "Fun Crate", "Classic Gnome Crate", "Statue Crate", "Beach Crate", "Summer Fun Crate", "Oasis Crate", "Exclusive Cosmetic Crate", "Twilight Crate", "July 4th Crate", "Bloodmoon Crate", "Bee Crate", "Honey Crafters Crate", "Iconic Gnome Crate"},
-  egg = { "Anti Bee Egg", "Bee Egg", "Bug Egg", "Common Egg", "Common Summer Egg", "Divine Egg", "Epic Egg", "Exotic Bug Egg", "Legendary Egg", "Mythical Egg", "Night Egg", "Paradise Egg", "Premium Anti Bee Egg", "Rare Egg", "Rare Summer Egg", "Uncommon Egg", "Oasis Egg" },
+  pet = {
+    "Axolotl", "Bald Eagle", "Bear Bee", "Bee", "Black Bunny", "Blood Hedgehog", "Blood Kiwi", "Blood Owl",
+    "Brontosaurus", "Brown Mouse", "Bunny", "Butterfly", "Capybara", "Caterpillar", "Cat", "Ceramic", "Chicken",
+    "Chicken Zombie", "Cooked Owl", "Cow", "Crab", "Deer", "Disco Bee", "Dog", "Echo Frog", "Fennec Fox", "Firefly",
+    "Flamingo", "Frog", "Giant Ant", "Golden Bee", "Golden Lab", "Grey Mouse", "Hamster", "Hedgehog", "Honey Bee",
+    "Hyacinth Macaw", "Kiwi", "Meerkat", "Mimic Octopus", "Mole", "Monkey", "Moon Cat", "Moth", "Night Owl",
+    "Orange Tabby", "Orangutan", "Ostrich", "Owl", "Pack Bee", "Panda", "Peacock", "Petal Bee", "Pig", "Polar Bear",
+    "Praying Mantis", "Pterodactyl", "Queen Bee", "Raccoon", "Raptor", "Red Fox", "Red Giant Ant", "Rooster",
+    "Sand Snake", "Scarlet Macaw", "Sea Otter", "Sea Turtle", "Seagull", "Seal", "Silver Monkey", "Snail",
+    "Spotted Deer", "Squirrel", "Starfish", "Stegosaurus", "T-Rex", "Tarantula Hawk", "Toucan", "Triceratops",
+    "Turtle", "Wasp"
+  },
+  crate = {"Dino Crate", "Mysterious Crate", "Sign Crate", "Common Gnome Crate", "Farmers Gnome Crate", "Fun Crate", "Classic Gnome Crate", "Statue Crate", "Beach Crate", "Summer Fun Crate", "Oasis Crate", "Exclusive Cosmetic Crate", "Twilight Crate", "July 4th Crate", "Bloodmoon Crate", "Bee Crate", "Honey Crafters Crate", "Iconic Gnome Crate" },
+  egg = { "Anti Bee Egg", "Bee Egg", "Bug Egg", "Common Egg", "Common Summer Egg", "Divine Egg", "Epic Egg", "Exotic Bug Egg", "Legendary Egg", "Mythical Egg", "Night Egg", "Paradise Egg", "Premium Anti Bee Egg", "Rare Egg", "Rare Summer Egg", "Uncommon Egg", "Oasis Egg", "Dinosaur Egg" },
   place_gear = { "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Master Sprinkler", "Lightning Rod", "Chocolate Sprinkler", "Tropical Sprinkler", "Berry Blusher Sprinkler", "Spice Spritzer Sprinkler", "Sweet Soaker Sprinkler", "Flower Froster Sprinkler", "Stalk Sprout Sprinkler", "Night Staff", "Star Caller", "Honey Sprinkler" },
   craft = {
     gear = {
@@ -36,42 +36,54 @@ local data = {
         recipe = { "Common Egg", "Harvest Tool" },
         cost = 500000,
         currency = "Sheckles",
-        timeToCraft = 2700,
+        timeToCraft = 1500,
         machine = "GearEventWorkbench"
       },
       ["Tropical Mist Sprinkler"] = {
         itemname = "Tropical Mist Sprinkler",
         recipe = { "Coconut", "Dragon Fruit", "Mango", "Godly Sprinkler" },
+        cost = nil,
+        currency = nil,
         timeToCraft = 3600,
         machine = "GearEventWorkbench"
       },
       ["Berry Blusher Sprinkler"] = {
         itemname = "Berry Blusher Sprinkler",
         recipe = { "Grape", "Blueberry", "Strawberry", "Godly Sprinkler" },
+        cost = nil,
+        currency = nil,
         timeToCraft = 3600,
         machine = "GearEventWorkbench"
       },
       ["Spice Spritzer Sprinkler"] = {
         itemname = "Spice Spritzer Sprinkler",
         recipe = { "Pepper", "Ember Lily", "Cacao", "Master Sprinkler" },
+        cost = nil,
+        currency = nil,
         timeToCraft = 3600,
         machine = "GearEventWorkbench"
       },
       ["Sweet Soaker Sprinkler"] = {
         itemname = "Sweet Soaker Sprinkler",
         recipe = { "Watermelon", "Watermelon", "Watermelon", "Master Sprinkler" },
+        cost = nil,
+        currency = nil,
         timeToCraft = 3600,
         machine = "GearEventWorkbench"
       },
       ["Flower Froster Sprinkler"] = {
         itemname = "Flower Froster Sprinkler",
         recipe = { "Orange Tulip", "Daffodil", "Advanced Sprinkler", "Basic Sprinkler" },
+        cost = nil,
+        currency = nil,
         timeToCraft = 3600,
         machine = "GearEventWorkbench"
       },
       ["Stalk Sprout Sprinkler"] = {
         itemname = "Stalk Sprout Sprinkler",
         recipe = { "Bamboo", "Beanstalk", "Mushroom", "Advanced Sprinkler" },
+        cost = nil,
+        currency = nil,
         timeToCraft = 3600,
         machine = "GearEventWorkbench"
       },
@@ -81,6 +93,14 @@ local data = {
         cost = 200000,
         currency = "Sheckles",
         timeToCraft = 720,
+        machine = "GearEventWorkbench"
+      },
+      ["Mutation Spray Chilled"] = {
+        itemname = "Mutation Spray Chilled",
+        recipe = { "Cleaning Spray", "Godly Sprinkler" },
+        cost = 500000,
+        currency = "Sheckles",
+        timeToCraft = 300,
         machine = "GearEventWorkbench"
       },
       ["Mutation Spray Pollinated"] = {
@@ -124,13 +144,63 @@ local data = {
         machine = "GearEventWorkbench"
       }
     },
-    seed = {
+    dino = {
+      ["Mutation Spray Amber"] = {
+        itemname = "Mutation Spray Amber",
+        recipe = { "Cleaning Spray", "Dinosaur Egg" },
+        cost = 1000000,
+        currency = "Sheckles",
+        timeToCraft = 3600,
+        machine = "DinoEventWorkbench"
+      },
+      ["Ancient Seed Pack"] = {
+        itemname = "Ancient Seed Pack",
+        recipe = { "Dinosaur Egg" },
+        cost = 5500000,
+        currency = "Sheckles",
+        timeToCraft = 3600,
+        machine = "DinoEventWorkbench"
+      },
+      ["Dino Crate"] = {
+        itemname = "Dino Crate",
+        recipe = { "Dinosaur Egg" },
+        cost = 5500000,
+        currency = "Sheckles",
+        timeToCraft = 1800,
+        machine = "DinoEventWorkbench"
+      }
+    },
+    seeds = {
       ["Crafters Seed Pack"] = {
         itemname = "Crafters Seed Pack",
         recipe = { "Flower Seed Pack" },
         cost = 10,
         currency = "Honey",
         timeToCraft = 1200,
+        machine = "SeedEventWorkbench"
+      },
+      ["Peace Lily"] = {
+        itemname = "Peace Lily",
+        recipe = { "Rafflesia", "Cauliflower" },
+        cost = 3,
+        currency = "SummerCoins",
+        timeToCraft = 600,
+        machine = "SeedEventWorkbench"
+      },
+      ["Aloe Vera"] = {
+        itemname = "Aloe Vera",
+        recipe = { "Peace Lily", "Prickly Pear" },
+        cost = 18,
+        currency = "SummerCoins",
+        timeToCraft = 600,
+        machine = "SeedEventWorkbench"
+      },
+      ["Guanabana"] = {
+        itemname = "Guanabana",
+        recipe = { "Aloe Vera", "Prickly Pear", "Banana" },
+        cost = 30,
+        currency = "SummerCoins",
+        timeToCraft = 600,
         machine = "SeedEventWorkbench"
       },
       ["Manuka Flower"] = {
