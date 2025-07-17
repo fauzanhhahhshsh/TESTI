cce0ac92e5f27ce9551d2a01b6cafe12a0cb1305
fauzanhhahhshsh <fauzannibosk@gmail.com>
2025-07-17 16:14:11 +0800

    Update LoaderV1.lua

--- Loader/LoaderV1.lua
+++ Loader/LoaderV1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fauzanhhahhshsh/TESTI/blob/main/Loader/Main.lua"))()
